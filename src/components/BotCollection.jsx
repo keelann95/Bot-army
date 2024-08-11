d94f4e7b63b3eb9df24e5db7a6bf5a1f0459c514
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-08-11 13:00:14 +0300

    changes the data in the display page to the correct data in the json file, also styles the data into cards

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -12,20 +12,25 @@ const BotCollection = () => {
 
   return (
     <div className=" flex  flex-wrap justify-evenly  min-h-screen bg-gray-100 p-4 gap-[6px]">
+      {bots.map(bot =>(
+        <> 
       <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-        <img 
-          className="w-full" 
-          src="https://robohash.org/nostrumrepellendustenetur.png?size=300x300&set=set1" 
-          alt="wHz-93" 
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">wHz-93</div>
-          <p className="text-gray-700 text-base mb-2">Support</p>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            more
-          </button>
-        </div>
+      <img 
+        className="w-full" 
+        src={bot.avatar_url}
+        alt={bot.name}
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{bot.name}</div>
+        <p className="text-gray-700 text-base mb-2">{bot.bot_class}</p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          more
+        </button>
       </div>
+    </div>
+    </>
+      ))}
+     
 
       
 
